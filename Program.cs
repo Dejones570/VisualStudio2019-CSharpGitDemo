ab4d99efd10a3edaa2ea0605ea213251154ed2de
Devon Jones <devonjones1@usf.edu>
2021-08-25 13:51:56 -0400

    Commit the chnages

--- Program.cs
+++ Program.cs
@@ -3,10 +3,14 @@
 namespace VisualStudio2019_CSharpGitDemo
 {
     class Program
-    {
-        static void Main(string[] args)
+
+    { 
+         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+        Console.WriteLine("Here is a change");
+
         }
     }
 }
